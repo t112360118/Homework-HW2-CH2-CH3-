c25f0be89863f010066daedeee88928f19b16874
t112360118 <t112360118@ntut.org.tw>
2025-11-12 14:33:25 +0800

    Add files via upload

---
+++ Chapter3/src/Sample3_4_1/Sample3_4_1.java
@@ -0,0 +1,24 @@
+package Sample3_4_1;
+
+public class Sample3_4_1 {
+    public static void main(String[] args) {
+        Car car1;        
+        car1 = new Car();    
+
+        car1.num = 1234;      
+        car1.gas = 20.5;       
+
+        car1.show();         
+        car1.show();          
+    }
+}
+
+class Car {
+    int num;
+    double gas;
+
+    void show() {            
+        System.out.println("車號是 " + num);
+        System.out.println("汽油量是 " + gas);
+    }
+}
---
+++ Chapter3/src/Sample3_7/Sample3_7.java
@@ -0,0 +1,29 @@
+package Sample3_7;
+
+public class Sample3_7 {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+
+        int number = 1234;
+        double gasoline = 20.5;
+
+        car1.setNumGas(number, gasoline);  
+        car1.show();
+    }
+}
+
+class Car {
+    int num;
+    double gas;
+
+    void setNumGas(int n, double g) {
+        num = n;
+        gas = g;
+        System.out.println("將車號設為 " + num + "，汽油量設為 " + gas);
+    }
+
+    void show() {
+        System.out.println("車號是 " + num);
+        System.out.println("汽油量是 " + gas);
+    }
+}
